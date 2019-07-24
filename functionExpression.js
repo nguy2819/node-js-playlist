5c1a876fc3c034c7aa07f7e0807c4b23716a3005
Tien Borland <nguy2819@crk.umn.edu>
2019-07-23 19:36:30 -0600

    function expressions

---
+++ functionExpression.js
@@ -0,0 +1,23 @@
+/* Normal Function Statement */
+
+function sayHi(){
+    console.log('hi');
+};
+
+sayHi();
+
+/* Call Function */
+
+function callFunction(fun){
+    fun();
+};
+
+/* Function Expression */
+
+var sayBye = function(){
+    console.log('bye');
+};
+
+// sayBye();
+
+callFunction(sayBye);//this use for the Call Function above
